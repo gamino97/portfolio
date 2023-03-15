c9c6eaa79fe16ef7578f1182cef98c3e36fd72a9
Carlos Gamino <carlos.alfredo55@gmail.com>
2023-03-15 14:10:17 -0600

    Add Hero Translation

---
+++ src/data/translations/hero.ts
@@ -0,0 +1,10 @@
+import type { Translation } from "./typings";
+
+const translations: Translation = {
+  heroTitle: {
+    en: "Full Stack Web Developer",
+    es: "Desarrollador Web Full Stack",
+  },
+};
+
+export default translations;
