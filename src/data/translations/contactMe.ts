fcf1704fc7ed92b3184e2c7af053941c482372ee
Carlos Gamino <carlos.alfredo55@gmail.com>
2023-03-18 15:37:15 -0600

    Add contactMe translations

---
+++ src/data/translations/contactMe.ts
@@ -0,0 +1,26 @@
+import type { Translation } from "./typings";
+
+const translations: Translation = {
+  title: {
+    en: "Contact Me",
+    es: "Contáctame",
+  },
+  nameInput: {
+    en: "Name",
+    es: "Nombre",
+  },
+  emailInput: {
+    en: "Email",
+    es: "Correo electrónico",
+  },
+  messageInput: {
+    en: "Message",
+    es: "Mensaje",
+  },
+  submitButton: {
+    en: "Submit",
+    es: "Enviar",
+  },
+};
+
+export default translations;
