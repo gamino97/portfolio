2911a01ee4d73b35913ab985a976a3aa1706aa16
Carlos Gamino <carlos.alfredo55@gmail.com>
2023-04-22 22:14:47 -0600

    Add Footer

---
+++ src/data/translations/footer.ts
@@ -0,0 +1,10 @@
+import type { Translation } from "./typings";
+
+const translations: Translation = {
+  text: {
+    en: "Made with ❤️ and 🚀Astro",
+    es: "Hecho con ❤️ y 🚀Astro",
+  },
+};
+
+export default translations;
