4a40cb0dc5285f1df5578c0295963f25ed11956f
Carlos Gamino <carlos.alfredo55@gmail.com>
2023-04-24 00:27:47 -0600

    Add astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,24 @@
 import { defineConfig } from "astro/config";
-
-// https://astro.build/config
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
 export default defineConfig({
+  site: "https://example.com",
   integrations: [
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    sitemap({
+      i18n: {
+        defaultLocale: "es", // All urls that don't contain `es` or `fr` after `https://stargazers.club/` will be treated as default locale, i.e. `en`
+        locales: {
+          en: "en", // The `defaultLocale` value must present in `locales` keys
+          es: "es",
+        },
+      },
     }),
   ],
 });
