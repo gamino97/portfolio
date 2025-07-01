f0d794592603960d8fa632a8693286a57aa90e44
Carlos Gamino <carlos.alfredo55@gmail.com>
2025-07-01 16:56:46 -0600

    Upgrade astro and astro-icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap";
@@ -6,5 +7,5 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.carlos-gamino.dev",
-  integrations: [sitemap()],
+  integrations: [sitemap(), icon()],
 });
